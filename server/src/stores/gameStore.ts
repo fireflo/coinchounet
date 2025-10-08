a6a989fa491953c1103510d941757f335a996f84
fireflo <flo.87.flo@gmail.com>
2025-10-08 08:53:06 -0400

    Missing game fields

--- server/src/stores/gameStore.ts
+++ server/src/stores/gameStore.ts
@@ -17,6 +17,11 @@ const cloneGame = (game: GameEntity): GameEntity => ({
   currentTurnIndex: game.currentTurnIndex,
   currentTrickOrder: [...game.currentTrickOrder],
   moveResults: new Map(game.moveResults),
+  biddingState: game.biddingState,
+  currentBidderIndex: game.currentBidderIndex,
+  completedTricks: [...game.completedTricks],
+  cumulativeScores: { ...game.cumulativeScores },
+  roundNumber: game.roundNumber,
 });
 
 export const gameStore = {
